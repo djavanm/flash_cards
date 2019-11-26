29d2c0cf410810f78e443b6ebcd46a1259969371
Djavan Munroe <djavanm24@gmail.com>
2019-11-25 18:39:02 -0700

    Fix test file for Deck and create deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,51 +1,7 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/card'
-require './lib/turn'
-require './lib/deck'
-require 'pry'
+class Deck
+  attr_accessor :cards
 
-class DeckTest < Minitest::Test
-
-  def test_it_exists
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-
-    assert_instance_of Deck, deck
-  end
-
-  def test_it_returns_the_cards_count
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-
-    assert_equal 3, deck.count
-  end
-
-  def test_it_has_an_array_of_cards
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-
-    assert_instance_of Card, deck.cards[0]
-  end
-
-  def test_it_returns_array_of_cards_given_a_category
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-
-    assert_equal 2, deck.cards_in_category(:STEM).length
-    asser_equal 0, deck.cards_in_category("Pop Culture").length
-    assert_equal "What is the capital of Alaska?", deck.cards_in_category(:Geography).question
+  def initialize cards
+    @cards = cards
   end
 end
